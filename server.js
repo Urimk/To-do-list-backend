5178e4fd096961713c04cacc916da77088024175
Uri <Umkil99@gmail.com>
2026-01-11 18:45:11 +0200

    added health check to ping

--- server.js
+++ server.js
@@ -18,11 +18,13 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-  
-
 app.use("/tasks", taskRoutes);
 app.use("/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "active", message: "Server is running" });
 });
